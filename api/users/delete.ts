2f4d62056b734a7ab07cde51f726c7f5306924b8
Zane Milakovic <zane.milakovic@icloud.com>
2019-04-12 20:50:03 -0500

    debugging delete

--- api/users/delete.ts
+++ api/users/delete.ts
@@ -4,6 +4,8 @@ import url from "url";
 export default (req: IncomingMessage, res: ServerResponse) => {
   res.writeHead(200, { "Content-Type": "application/json" });
 
+  console.log('delete called', req.url);
+
   try {
     let { query } = url.parse(req.url || "", true)
     res.end(JSON.stringify({ message: `Query Sent: ${query}` }));
