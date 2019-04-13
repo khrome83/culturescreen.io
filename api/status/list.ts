1127a37e7e76de0968fa42312a2e8baaec4d42d7
Zane Milakovic <zane.milakovic@icloud.com>
2019-04-12 19:37:06 -0500

    switches a endpoint to ts

---
+++ api/status/list.ts
@@ -0,0 +1,6 @@
+import { IncomingMessage, ServerResponse } from "http";
+
+export default (req: IncomingMessage, res: ServerResponse) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ message: 'I am awake!' }));
+};
