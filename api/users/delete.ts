6792abd6cb5b1951d1b9832b531c8ca6996c0f8f
Zane Milakovic <zane.milakovic@icloud.com>
2019-04-12 20:45:34 -0500

    converts all endpoints to typescript

---
+++ api/users/delete.ts
@@ -0,0 +1,16 @@
+import { IncomingMessage, ServerResponse } from "http";
+import url from "url";
+
+export default (req: IncomingMessage, res: ServerResponse) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+
+  try {
+    let { query } = url.parse(req.url || "", true)
+    res.end(JSON.stringify({ message: `Query Sent: ${query}` }));
+  } catch (error) {
+    res.end(JSON.stringify({
+      message: 'Something went wrong.',
+      trace: error,
+    }));
+  }
+};
