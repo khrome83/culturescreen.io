d534e1cbe532d43013e50ae73198372e824f3c5a
Zane Milakovic <zane.milakovic@icloud.com>
2019-04-12 21:24:46 -0500

    updated delete method to remove incorrect data

--- api/users/delete.ts
+++ api/users/delete.ts
@@ -4,12 +4,9 @@ import url from "url";
 export default (req: IncomingMessage, res: ServerResponse) => {
   res.writeHead(200, { "Content-Type": "application/json" });
 
-  console.log('delete called', req.url);
-
   try {
     let { query } = url.parse(req.url || "", true)
-    console.log(`query: ${JSON.stringify(query)}`);
-    res.end(JSON.stringify({ message: `Query Sent: ${JSON.stringify(query)}` }));
+    res.end(JSON.stringify({ message: `Deleted User ${query.id}` }));
   } catch (error) {
     res.end(JSON.stringify({
       message: 'Something went wrong.',
