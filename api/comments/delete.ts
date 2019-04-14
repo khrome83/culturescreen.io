5b8b1872f63c1d11edac48254e4b131c370a60c4
Zane Milakovic <zane.milakovic@icloud.com>
2019-04-13 20:26:29 -0500

    stubs out comments endpoint

---
+++ api/comments/delete.ts
@@ -0,0 +1,16 @@
+import { IncomingMessage, ServerResponse } from "http";
+import url from "url";
+
+export default (req: IncomingMessage, res: ServerResponse) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+
+  try {
+    let { query } = url.parse(req.url || "", true)
+    res.end(JSON.stringify({ message: `Deleted Comment ${query.id}` }));
+  } catch (error) {
+    res.end(JSON.stringify({
+      message: 'Something went wrong.',
+      trace: error,
+    }));
+  }
+};
