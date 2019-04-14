bf75a12e4c0bd5032629ee62dda64565b32cb67b
Zane Milakovic <zane.milakovic@icloud.com>
2019-04-14 18:16:04 -0500

    testing the body parse

--- api/comments/create.ts
+++ api/comments/create.ts
@@ -40,6 +40,8 @@ export default async (req: IncomingMessage, res: ServerResponse) => {
   try {
     const body = await json(req);
 
+    send(res, 200, body);
+
     const item = ({
       id: uuid(),
       date: new Date().toISOString(),
