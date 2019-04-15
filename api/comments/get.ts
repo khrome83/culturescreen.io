07106630e812866deec137d00e5eab26c01b1fd3
Zane Milakovic <zane.milakovic@icloud.com>
2019-04-15 06:40:08 -0500

    adds support for getting a comment

--- api/comments/get.ts
+++ api/comments/get.ts
@@ -1,7 +1,51 @@
 import { IncomingMessage, ServerResponse } from "http";
+import url from "url";
+import { CosmosClient, SqlQuerySpec } from "@azure/cosmos";
+import { send } from "micro";
+import { v4 } from "uuid/interfaces";
+import getOptions, { Databases, Containers, Mode } from "../connectors/cosmos";
+import errorHandler from "../utils/errorHandler";
 
-// Random Update
-export default (req: IncomingMessage, res: ServerResponse) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ message: "retrieved comment" }));
+interface Reaction {
+  type: string;
+  author: v4;
+}
+
+interface Comment {
+  id: v4;
+  author: v4;
+  video: v4;
+  comment: string;
+  date: string;
+  edited: boolean;
+  editedDate?: string;
+  reactions?: Array<Reaction>;
+}
+
+export default async (req: IncomingMessage, res: ServerResponse) => {
+  const client = new CosmosClient(getOptions(Mode.Write));
+
+  try {
+    const { query } = url.parse(req.url || "", true);
+
+    const querySpec = {
+      query: "SELECT * FROM root r WHERE r.id = @id",
+      parameters: [
+        {
+          name: "@id",
+          value: query.id
+        }
+      ]
+    } as SqlQuerySpec;
+
+    const { result: results = [] } = await client
+      .database(Databases.Primary)
+      .container(Containers.Comments)
+      .items.query(querySpec)
+      .toArray();
+
+    send(res, 200, results);
+  } catch (error) {
+    send(res, 500, errorHandler(500, new Error("Unable to create comment.")));
+  }
 };
