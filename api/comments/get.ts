057d045494abc9570022777155e43e283888d033
Zane Milakovic <zane.milakovic@icloud.com>
2019-04-14 20:47:56 -0500

    random update to trigger build

--- api/comments/get.ts
+++ api/comments/get.ts
@@ -1,5 +1,6 @@
 import { IncomingMessage, ServerResponse } from "http";
 
+// Random Update
 export default (req: IncomingMessage, res: ServerResponse) => {
   res.writeHead(200, { "Content-Type": "application/json" });
   res.end(JSON.stringify({ message: "retrieved comment" }));
