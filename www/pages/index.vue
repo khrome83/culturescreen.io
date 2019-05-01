946912948861ef057aab4d26ed327d233dae3ae7
Zane Milakovic <zane.milakovic@icloud.com>
2019-05-01 14:53:02 -0500

    removes duplicate feilds

--- www/pages/index.vue
+++ www/pages/index.vue
@@ -1,47 +1,14 @@
 <template>
   <section>
-    <h1 class="header">Users</h1>
-    <div class="users">
-      <UserRow v-for="user in users" :key="user.id" :user="user"></UserRow>
-    </div>
-    <NewUser />
-    <h3>Robot Cards</h3>
-    <div class="cards">
-      <Card v-for="person in people" :key="person.id" :person="person"></Card>
-    </div>
-    <TheFooter />
+    <h1 class="header">Content</h1>
   </section>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Action } from "nuxt-property-decorator";
-import { State } from "vuex-class";
-import { Person, User } from "~/types";
-import TheFooter from "~/components/TheFooter.vue";
+import { Component, Vue } from "nuxt-property-decorator";
 
-@Component({
-  components: {
-    TheFooter
-  }
-})
-export default class extends Vue {
-  @State people!: Person;
-  @State users!: User;
-  @Action bindUsers;
-
-  created() {
-    this.bindUsers();
-  }
-}
+@Component({})
+export default class extends Vue {}
 </script>
 
-<style scoped>
-.header {
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-}
-
-.cards {
-  display: flex;
-  flex-wrap: wrap;
-}
-</style>
+<style scoped></style>
