591d8d175c94b1c2b7cb380cbd44b65856400082
Zane Milakovic <zane.milakovic@icloud.com>
2019-05-10 21:40:19 -0500

    shows base buttons on homepage

--- www/pages/index.vue
+++ www/pages/index.vue
@@ -1,13 +1,26 @@
 <template>
   <section>
     <h1 class="header">Content</h1>
+    <base-button overpanel="Default">Open Default Overpanel</base-button>
+    <base-button target="_blank" alt>Testing</base-button>
+    <base-button to="https://www.example.com">Primary</base-button>
+    <base-button title="secondary" to="https://www.example.com" secondary
+      >Secondary</base-button
+    >
+    <base-button to="https://www.example.com" tertiary>Tertiary</base-button>
+    <base-button to="/signup" danger>Danger</base-button>
   </section>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "nuxt-property-decorator";
+import BaseButton from "~/components/BaseButton.vue";
 
-@Component({})
+@Component({
+  components: {
+    BaseButton
+  }
+})
 export default class extends Vue {}
 </script>
 
