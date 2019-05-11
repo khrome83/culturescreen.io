5e3524d2d20082a1a93f58a4b3ee1fd600773e19
Zane Milakovic <zane.milakovic@icloud.com>
2019-05-10 21:34:57 -0500

    adds default overpanel conents

---
+++ www/components/overpanel/Default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="test">TESTING THIS THING</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+
+export default class DefaultOverpanel extends Vue {}
+</script>
+
+<style scoped>
+.form-control { color: pink; }
+</style>
