15c2ad6ea6d9280a08e39c659c2120f4a28f9c8f
Zane Milakovic <zane.milakovic@icloud.com>
2019-05-13 10:35:16 -0500

    fixed formatting in overpanel

--- www/components/TheOverPanel.vue
+++ www/components/TheOverPanel.vue
@@ -62,49 +62,139 @@ export default class TheOverPanel extends Vue {
 }
 </script>
 
-<style>
-.overpanel-mask { position: fixed; z-index: 9998; top: 0; right: 0; bottom: 0;
-  left: 0; overflow: hidden; background-color: rgba(0, 0, 0, 0.5); transition: background-color 0.3s ease; }
-
-.overpanel-container { position: absolute;
-  z-index: 9999; top: 0; right: 0; bottom: 0; width: 60rem; background-color: #fff; padding: 4rem 2rem 4rem 6rem; transform-origin: right; transform: translateX(0); transition: transform 0.3s ease; }
-
-.overpanel-header { position: absolute; top: 0; bottom: 0; left: 0; width: 4rem; background: linear-gradient(
-  to top right, #35495e, #3a5774, #40658a, #4573a1, #4b81b9 ); }
-
-.header-text {
-  display: inline-block; writing-mode: tb-rl; color: #fff; font-size: 2rem;
-  line-height: 2; padding: 0 0 2rem 0; width: 100%; white-space: nowrap;
-  letter-spacing: 0.2rem; text-transform: uppercase; }
-
-.close { position: absolute; display: block; background: transparent; border: none; top: 1rem;
-  right: 1rem; fill: #40658a; cursor: pointer; margin: 0; padding: 0.5rem;
-  border-radius: 100%; border: 0.185rem solid transparent; outline: 0; }
-
-.close::before { content: "close"; display: block; position: absolute; width: 3rem; color: transparent; margin-left: -3.5rem; line-height: 1.6; }
-
-.close:hover::before, .close:active::before { color: #40658a; }
-
-.close:hover,
-.close:active { fill: #4b81b9; border-color: #4b81b9; }
-
-.close svg { width: 1.5rem; height: 1.3rem; }
-
-.overpanel-header h3 { margin-top: 0; color: #42b983;
+<style scoped>
+.overpanel-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  overflow: hidden;
+  background-color: rgba(0, 0, 0, 0.5);
+  transition: background-color 0.3s ease;
 }
 
-.overpanel-body { margin: 1.25rem 0; }
+.overpanel-container {
+  position: absolute;
+  z-index: 9999;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  width: 60rem;
+  background-color: #fff;
+  padding: 4rem 2rem 4rem 6rem;
+  transform-origin: right;
+  transform: translateX(0);
+  transition: transform 0.3s ease;
+}
 
-.overpanel-content { display: flex;
-  flex-direction: column; justify-content: space-between; min-height: 100%; }
+.overpanel-header {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  width: 4rem;
+  background: linear-gradient(
+  to top right,
+  #35495e,
+  #3a5774,
+  #40658a,
+  #4573a1,
+  #4b81b9
+);
+}
 
-.overpanel-enter, .overpanel-leave-to { background-color: rgba(0, 0, 0, 0); }
+.header-text {
+  display: inline-block;
+  writing-mode: tb-rl;
+  color: #fff;
+  font-size: 2rem;
+  line-height: 2;
+  padding: 0 0 2rem 0;
+  width: 100%;
+  white-space: nowrap;
+  letter-spacing: 0.2rem;
+  text-transform: uppercase;
+}
 
-.overpanel-enter-to, .overpanel-leave { background-color: rgba(0, 0, 0, 0.5); }
+.close {
+  position: absolute;
+  display: block;
+  background: transparent;
+  border: none;
+  top: 1rem;
+  right: 1rem;
+  fill: #40658a;
+  cursor: pointer;
+  margin: 0;
+  padding: 0.5rem;
+  border-radius: 100%;
+  border: 0.185rem solid transparent;
+  outline: 0;
+}
 
-.overpanel-enter .overpanel-container, .overpanel-leave-to .overpanel-container
-{ transform-origin: right; transform: translateX(60rem); }
+.close::before {
+  content: "close";
+  display: block;
+  position: absolute;
+  width: 3rem;
+  color: transparent;
+  margin-left: -3.5rem;
+  line-height: 1.6;
+}
 
-.overpanel-enter-to
-.overpanel-container, .overpanel-leave .overpanel-container { transform-origin: right; transform: translateX(0); }
+.close:hover::before,
+.close:active::before {
+  color: #40658a;
+}
+
+.close:hover,
+.close:active {
+  fill: #4b81b9;
+  border-color: #4b81b9;
+}
+
+.close svg {
+  width: 1.5rem;
+  height: 1.3rem;
+}
+
+.overpanel-header h3 {
+  margin-top: 0;
+  color: #42b983;
+}
+
+.overpanel-body {
+  margin: 1.25rem 0;
+}
+
+.overpanel-content {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 100%;
+}
+
+.overpanel-enter,
+.overpanel-leave-to {
+  background-color: rgba(0, 0, 0, 0);
+}
+
+.overpanel-enter-to,
+.overpanel-leave {
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.overpanel-enter .overpanel-container,
+.overpanel-leave-to .overpanel-container {
+  transform-origin: right;
+  transform: translateX(60rem);
+}
+
+.overpanel-enter-to .overpanel-container,
+.overpanel-leave .overpanel-container {
+  transform-origin: right;
+  transform: translateX(0);
+}
 </style>
