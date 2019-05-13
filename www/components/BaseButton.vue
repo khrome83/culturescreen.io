bf94f783b91eac1a512aba7846c5c496765eac0b
Zane Milakovic <zane.milakovic@icloud.com>
2019-05-13 07:56:29 -0500

    refactored the button for disabled and icon states

--- www/components/BaseButton.vue
+++ www/components/BaseButton.vue
@@ -4,9 +4,11 @@
     v-bind="linkProps.bindProps"
     v-on="linkProps.onEvents"
     class="button"
-    :class="{ secondary, tertiary, danger }"
+    :class="{ secondary, tertiary, danger, disabled }"
   >
+    <slot name="pre-icon"></slot>
     <slot></slot>
+    <slot name="post-icon"></slot>
   </component>
 </template>
 
@@ -22,11 +24,16 @@ export default class BaseButton extends Vue {
   @Prop(Boolean) private secondary!: boolean;
   @Prop(Boolean) tertiary!: boolean;
   @Prop(Boolean) danger!: boolean;
+  @Prop(Boolean) disabled!: boolean;
   @Prop() overpanel!: string;
 
   @overpanel.Mutation("openOverpanel") openOverpanel;
 
   get element() {
+    if (this.overpanel || this.to === "#") {
+      return "button";
+    }
+
     if (
       /((https{0,1}|ftp|tel|mail):\/\/)|^www.|.{1}(com|net|org|io)/i.test(
         this.to
@@ -35,79 +42,79 @@ export default class BaseButton extends Vue {
       return "a";
     }
 
-    if (this.overpanel) {
-      return "button";
-    }
-
     return "nuxt-link";
   }
 
   get linkProps() {
-    if (this.element === "a") {
-      return {
-        onEvents: {},
-        bindProps: {
-          href: this.to,
-          target: "_blank",
-          rel: "noopener",
-          title: this.title
-        }
-      };
-    }
-
-    if (this.overpanel) {
-      return {
-        onEvents: {
-          click: () => {
-            this.openOverpanel(this.overpanel);
-          }
-        },
-        bindProps: {}
-      };
-    }
-
-    return {
-      onEvents: {},
-      bindProps: {
-        to: this.to,
-        title: this.title
-      }
+    const onEvents = {};
+    const bindProps = {
+      title: this.title
     };
+
+    if (this.element === "a") {
+      bindProps["href"] = this.to;
+      bindProps["target"] = "_blank";
+      bindProps["rel"] = "noopener";
+    } else if (this.overpanel) {
+      onEvents["click"] = () => {
+        this.openOverpanel(this.overpanel);
+      };
+    } else if (this.element === "nuxt-link") {
+      bindProps["to"] = this.to;
+    }
+
+    if (this.disabled === true) {
+      bindProps["aria-disabled"] = true;
+      bindProps["disabled"] = true;
+      delete onEvents["click"];
+    }
+
+    return { onEvents, bindProps };
   }
 }
 </script>
 
 <style scoped>
-.button, .button:hover, .button:active { position: relative; border: 0.125rem
-  solid transparent; border-radius: 0.25rem; background-color: transparent;
-  padding: 0.8rem 2rem; display: inline-block; font-size: 1.2rem; text-decoration: none; cursor: pointer; color: transparent; background: linear-gradient( to right
-bottom, #3b8070, #398e77, #399c7c, #3caa80, #41b883 ); -webkit-background-clip: text; background-clip: text; -webkit-text-fill-color: transparent;
-  text-fill-color: transparent; }
+.button, .button:hover, .button:active { display: inline-block; text-align: center; color: #fff; fill: #fff; background-color: #ed303c; border-radius: 0.25rem; padding: 0.25rem 1rem; text-decoration: none; font-family: "Raleway",
+  sans-serif; font-size: 1.125rem; font-weight: 300; line-height: 1.5;
+  letter-spacing: 0.025rem; cursor: pointer; flex-basis: 100%; border: thin solid
+  #ed303c; transition: all 200ms ease-in-out; }
 
-.button::before { position: absolute; top: 0;
-  right: 0; bottom: 0; left: 0; background: linear-gradient( to right bottom,
-#3b8070, #398e77, #399c7c, #3caa80, #41b883 ); content: ""; z-index: -2;
-  border-radius: 0.25rem; }
+.button:hover, .button:active,
+.button:focus { background-color: #fff; color: #ed303c; fill: #ed303c;
+  transition: all 200ms ease-in-out; }
 
-.button::after { position: absolute; top: 0; right: 0;
-  bottom: 0; left: 0; background: #fff; content: ""; z-index: -1; border-radius: 0.25rem; border: 0.125rem solid transparent; background-clip: padding-box; }
+.button:focus { outline: 0; box-shadow: inset 0 0 0 0.0625rem #fff, inset 0 0 0 0.125rem #ed303c; }
 
-.button:hover::after { border: 0.25rem solid transparent; }
+.secondary {
+  border-color: #010b19; background-color: #010b19; color: #fff; fill: #fff; }
 
-.button.secondary,
-.button.secondary:hover, .button.secondary:active { color: transparent;
-  background: linear-gradient( to right bottom, #35495e, #3a5774, #40658a,
-#4573a1, #4b81b9 ); -webkit-background-clip: text; background-clip: text;
-  background-color: text; -webkit-text-fill-color: transparent; text-fill-color: transparent; }
+.secondary:hover, .secondary:active, .secondary:focus { background-color: transparent; color: #010b19; fill: #010b19; border-color: #010b19; }
 
-.button.secondary::before { background: linear-gradient( to right
-bottom, #35495e, #3a5774, #40658a, #4573a1, #4b81b9 ); }
+.secondary:focus { box-shadow: inset 0 0 0 0.0625rem #fff, inset 0 0 0 0.125rem
+  #010b19; }
 
-.button.danger,
-.button.danger:hover, .button.danger:active { color: transparent; background: linear-gradient( to right bottom, #922648, #a92851, #c12959, #d92a61, #f22b69 );
-  -webkit-background-clip: text; background-clip: text; -webkit-text-fill-color: transparent; text-fill-color: transparent; }
+.tertiary { background-color: transparent; color: #5c6169; fill: #5c6169; border: thin solid #a0a9ba; }
 
-.button.danger::before {
-  background: linear-gradient( to right bottom, #922648, #a92851, #c12959,
-#d92a61, #f22b69 ); }
+.tertiary:hover, .tertiary:active,
+.tertiary:focus { border-color: #010b19; color: #010b19; fill: #010b19; }
+
+.tertiary:focus { box-shadow: inset 0 0 0 0.0625rem #fff, inset 0 0 0 0.125rem
+  #010b19; }
+
+.danger { background-color: transparent; border-color: transparent;
+  color: #ed303c; fill: #ed303c; }
+
+.danger:hover, .danger:active {
+  text-decoration: underline; color: #ed303c; fill: #ed303c; border-color: transparent; }
+
+.disabled, .disabled:hover, .disabled:active, .disabled:focus {
+  background-color: #fafafa; border-color: #eaeaea; color: #cccccc; fill: #cccccc;
+  cursor: not-allowed; text-decoration: none; }
+
+.disabled:focus { box-shadow: inset 0 0 0 0.0625rem #fafafa, inset 0 0 0 0.125rem #eaeaea; }
+</style>
+<style>
+.button > svg { height: 1rem; width: 1rem; transform: scale(1.5); margin: 0
+  0.125rem -0.2rem; }
 </style>
