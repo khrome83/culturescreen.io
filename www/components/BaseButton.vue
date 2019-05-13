ffea787d8c569d7eb4aed7645b2fbb286afb89cb
Zane Milakovic <zane.milakovic@icloud.com>
2019-05-13 08:06:17 -0500

    improves focus transitions

--- www/components/BaseButton.vue
+++ www/components/BaseButton.vue
@@ -75,46 +75,121 @@ export default class BaseButton extends Vue {
 </script>
 
 <style scoped>
-.button, .button:hover, .button:active { display: inline-block; text-align: center; color: #fff; fill: #fff; background-color: #ed303c; border-radius: 0.25rem; padding: 0.25rem 1rem; text-decoration: none; font-family: "Raleway",
-  sans-serif; font-size: 1.125rem; font-weight: 300; line-height: 1.5;
-  letter-spacing: 0.025rem; cursor: pointer; flex-basis: 100%; border: thin solid
-  #ed303c; transition: all 200ms ease-in-out; }
+.button,
+.button:hover,
+.button:active {
+  display: inline-block;
+  text-align: center;
+  color: #fff;
+  fill: #fff;
+  background-color: #ed303c;
+  border-radius: 0.25rem;
+  padding: 0.25rem 1rem;
+  text-decoration: none;
+  font-family: "Raleway", sans-serif;
+  font-size: 1.125rem;
+  font-weight: 300;
+  line-height: 1.5;
+  letter-spacing: 0.025rem;
+  cursor: pointer;
+  flex-basis: 100%;
+  border: thin solid #ed303c;
+  transition: all 200ms ease-in-out;
+}
 
-.button:hover, .button:active,
-.button:focus { background-color: #fff; color: #ed303c; fill: #ed303c;
-  transition: all 200ms ease-in-out; }
+.button:hover,
+.button:active,
+.button:focus {
+  background-color: #fff;
+  color: #ed303c;
+  fill: #ed303c;
+  transition: all 200ms ease-in-out;
+  box-shadow: inset 0 0 0 0 #fff, inset 0 0 0 0 #ed303c;
+}
 
-.button:focus { outline: 0; box-shadow: inset 0 0 0 0.0625rem #fff, inset 0 0 0 0.125rem #ed303c; }
+.button:focus {
+  outline: 0;
+  box-shadow: inset 0 0 0 0.0625rem #fff, inset 0 0 0 0.125rem #ed303c;
+}
 
 .secondary {
-  border-color: #010b19; background-color: #010b19; color: #fff; fill: #fff; }
+  border-color: #010b19;
+  background-color: #010b19;
+  color: #fff;
+  fill: #fff;
+}
 
-.secondary:hover, .secondary:active, .secondary:focus { background-color: transparent; color: #010b19; fill: #010b19; border-color: #010b19; }
+.secondary:hover,
+.secondary:active,
+.secondary:focus {
+  background-color: transparent;
+  color: #010b19;
+  fill: #010b19;
+  border-color: #010b19;
+  box-shadow: inset 0 0 0 0 #fff, inset 0 0 0 0 #010b19;
+}
 
-.secondary:focus { box-shadow: inset 0 0 0 0.0625rem #fff, inset 0 0 0 0.125rem
-  #010b19; }
+.secondary:focus {
+  box-shadow: inset 0 0 0 0.0625rem #fff, inset 0 0 0 0.125rem #010b19;
+}
 
-.tertiary { background-color: transparent; color: #5c6169; fill: #5c6169; border: thin solid #a0a9ba; }
+.tertiary {
+  background-color: transparent;
+  color: #5c6169;
+  fill: #5c6169;
+  border: thin solid #a0a9ba;
+}
 
-.tertiary:hover, .tertiary:active,
-.tertiary:focus { border-color: #010b19; color: #010b19; fill: #010b19; }
+.tertiary:hover,
+.tertiary:active,
+.tertiary:focus {
+  border-color: #010b19;
+  color: #010b19;
+  fill: #010b19;
+  box-shadow: inset 0 0 0 0 #fff, inset 0 0 0 0 #010b19;
+}
 
-.tertiary:focus { box-shadow: inset 0 0 0 0.0625rem #fff, inset 0 0 0 0.125rem
-  #010b19; }
+.tertiary:focus {
+  box-shadow: inset 0 0 0 0.0625rem #fff, inset 0 0 0 0.125rem #010b19;
+}
 
-.danger { background-color: transparent; border-color: transparent;
-  color: #ed303c; fill: #ed303c; }
+.danger {
+  background-color: transparent;
+  border-color: transparent;
+  color: #ed303c;
+  fill: #ed303c;
+}
 
-.danger:hover, .danger:active {
-  text-decoration: underline; color: #ed303c; fill: #ed303c; border-color: transparent; }
+.danger:hover,
+.danger:active {
+  text-decoration: underline;
+  color: #ed303c;
+  fill: #ed303c;
+  border-color: transparent;
+}
 
-.disabled, .disabled:hover, .disabled:active, .disabled:focus {
-  background-color: #fafafa; border-color: #eaeaea; color: #cccccc; fill: #cccccc;
-  cursor: not-allowed; text-decoration: none; }
+.disabled,
+.disabled:hover,
+.disabled:active,
+.disabled:focus {
+  background-color: #fafafa;
+  border-color: #eaeaea;
+  color: #cccccc;
+  fill: #cccccc;
+  cursor: not-allowed;
+  text-decoration: none;
+  box-shadow: inset 0 0 0 0 #fafafa, inset 0 0 0 0 #eaeaea;
+}
 
-.disabled:focus { box-shadow: inset 0 0 0 0.0625rem #fafafa, inset 0 0 0 0.125rem #eaeaea; }
+.disabled:focus {
+  box-shadow: inset 0 0 0 0.0625rem #fafafa, inset 0 0 0 0.125rem #eaeaea;
+}
 </style>
 <style>
-.button > svg { height: 1rem; width: 1rem; transform: scale(1.5); margin: 0
-  0.125rem -0.2rem; }
+.button > svg {
+  height: 1rem;
+  width: 1rem;
+  transform: scale(1.5);
+  margin: 0 0.125rem -0.2rem;
+}
 </style>
