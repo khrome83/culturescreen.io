570c9bed86dcb8aa07c8ba16e6cc7a15e8924313
Zane Milakovic <zane.milakovic@icloud.com>
2019-05-14 10:18:12 -0500

    updates the base secetion with more functionality

--- www/components/BaseSection.vue
+++ www/components/BaseSection.vue
@@ -1,6 +1,6 @@
 <template>
-  <section>
-    <div class="container">
+  <section :class="theme">
+    <div class="container" :class="layout">
       <slot />
     </div>
   </section>
@@ -9,15 +9,23 @@
 <script lang="ts">
 import { Component, Prop, Vue } from "nuxt-property-decorator";
 
+export enum Layouts {
+  Small = "small",
+  Medium = "medium",
+  Large = "large"
+}
+
+export enum Themes {
+  Light = "light",
+  Grey = "grey",
+  Dark = "dark"
+}
+
 @Component({})
-export default class BaseButton extends Vue {
-  @Prop({ default: "#" }) to!: string;
-  @Prop() title!: string;
-  @Prop(Boolean) private secondary!: boolean;
-  @Prop(Boolean) tertiary!: boolean;
-  @Prop(Boolean) danger!: boolean;
-  @Prop(Boolean) disabled!: boolean;
-  @Prop() overpanel!: string;
+export default class BaseSection extends Vue {
+  @Prop({ default: Layouts.Medium })
+  private layout!: Layouts;
+  @Prop({ default: Themes.Light }) private theme!: Themes;
 }
 </script>
 
@@ -30,5 +38,30 @@ section {
   padding: 0.5rem 2.5vw 1.5rem;
   max-width: 90rem;
   margin: 0 auto;
+}
+
+.small {
+  padding: 2rem 0;
+}
+
+.medium {
+  padding: 4rem 0;
+}
+
+.large {
+  padding: 6rem 0;
+}
+
+.light {
+  background-color: #fff;
+}
+
+.grey {
+  background-color: #e1e4ea;
+}
+
+.dark {
+  background-color: #010b19;
+  color: #fff;
 }
 </style>
--- www/pages/styleguide.vue
+++ www/pages/styleguide.vue
@@ -1,116 +1,130 @@
 <template>
-  <base-section>
-    <h1 class="header">Styleguide</h1>
+  <div>
+    <base-section>
+      <h1 class="header">Styleguide</h1>
 
-    <h2>Button Elements</h2>
+      <h2>Button Elements</h2>
 
-    <h3>Overpanel Buttons</h3>
-    <base-button overpanel="Default">Primary</base-button>
-    <base-button overpanel="Default" secondary>Secondary</base-button>
-    <base-button overpanel="Default" tertiary>Tertiary</base-button>
-    <base-button overpanel="Default" danger>Danger</base-button>
-    <h4>Disabled</h4>
-    <base-button overpanel="Default" disabled>Primary</base-button>
-    <base-button overpanel="Default" secondary disabled>Secondary</base-button>
-    <base-button overpanel="Default" tertiary disabled>Tertiary</base-button>
-    <base-button overpanel="Default" danger disabled>Danger</base-button>
-    <h3>Custom Events</h3>
-    <base-button @click.native="test">Primary</base-button>
-    <base-button @click.native="test" secondary>Secondary</base-button>
-    <base-button @click.native="test" tertiary>Tertiary</base-button>
-    <base-button @click.native="test" danger>Danger</base-button>
-    <h4>Disabled</h4>
-    <base-button @click.native="test" disabled>Primary</base-button>
-    <base-button @click.native="test" secondary disabled>Secondary</base-button>
-    <base-button @click.native="test" tertiary disabled>Tertiary</base-button>
-    <base-button @click.native="test" danger disabled>Danger</base-button>
-    <h3>External Link</h3>
-    <base-button to="https://www.example.com">Primary</base-button>
-    <base-button title="secondary" to="https://www.example.com" secondary
-      >Secondary</base-button
+      <h3>Overpanel Buttons</h3>
+      <base-button overpanel="Default">Primary</base-button>
+      <base-button overpanel="Default" secondary>Secondary</base-button>
+      <base-button overpanel="Default" tertiary>Tertiary</base-button>
+      <base-button overpanel="Default" danger>Danger</base-button>
+      <h4>Disabled</h4>
+      <base-button overpanel="Default" disabled>Primary</base-button>
+      <base-button overpanel="Default" secondary disabled
+        >Secondary</base-button
+      >
+      <base-button overpanel="Default" tertiary disabled>Tertiary</base-button>
+      <base-button overpanel="Default" danger disabled>Danger</base-button>
+      <h3>Custom Events</h3>
+      <base-button @click.native="test">Primary</base-button>
+      <base-button @click.native="test" secondary>Secondary</base-button>
+      <base-button @click.native="test" tertiary>Tertiary</base-button>
+      <base-button @click.native="test" danger>Danger</base-button>
+      <h4>Disabled</h4>
+      <base-button @click.native="test" disabled>Primary</base-button>
+      <base-button @click.native="test" secondary disabled
+        >Secondary</base-button
+      >
+      <base-button @click.native="test" tertiary disabled>Tertiary</base-button>
+      <base-button @click.native="test" danger disabled>Danger</base-button>
+      <h3>External Link</h3>
+      <base-button to="https://www.example.com">Primary</base-button>
+      <base-button title="secondary" to="https://www.example.com" secondary
+        >Secondary</base-button
+      >
+      <base-button to="https://www.example.com" tertiary>Tertiary</base-button>
+      <base-button to="/signup" danger>Danger</base-button>
+      <h4>Disabled</h4>
+      <base-button to="https://www.example.com" disabled>Primary</base-button>
+      <base-button
+        title="secondary"
+        to="https://www.example.com"
+        secondary
+        disabled
+        >Secondary</base-button
+      >
+      <base-button to="https://www.example.com" tertiary disabled
+        >Tertiary</base-button
+      >
+      <base-button to="https://www.example.com" danger disabled
+        >Danger</base-button
+      >
+      <h3>Left Icons</h3>
+      <base-button to="https://www.example.com">
+        <heart-icon slot="pre-icon" />Primary
+      </base-button>
+      <base-button title="secondary" to="https://www.example.com" secondary>
+        <heart-icon slot="pre-icon" />Secondary
+      </base-button>
+      <base-button to="https://www.example.com" tertiary>
+        <heart-icon slot="pre-icon" />Tertiary
+      </base-button>
+      <base-button to="https://www.example.com" danger>
+        <heart-icon slot="pre-icon" />Danger
+      </base-button>
+      <h3>Right Icons</h3>
+      <base-button to="https://www.example.com">
+        <heart-icon slot="post-icon" />Primary
+      </base-button>
+      <base-button title="secondary" to="https://www.example.com" secondary>
+        <heart-icon slot="post-icon" />Secondary
+      </base-button>
+      <base-button to="https://www.example.com" tertiary>
+        <heart-icon slot="post-icon" />Tertiary
+      </base-button>
+      <base-button to="https://www.example.com" danger>
+        <heart-icon slot="post-icon" />Danger
+      </base-button>
+      <br />
+      <br />
+      <base-button to="https://www.example.com" disabled>
+        <heart-icon slot="post-icon" />Primary
+      </base-button>
+      <base-button
+        title="secondary"
+        to="https://www.example.com"
+        secondary
+        disabled
+      >
+        <heart-icon slot="post-icon" />Secondary
+      </base-button>
+      <base-button to="https://www.example.com" tertiary disabled>
+        <heart-icon slot="post-icon" />Tertiary
+      </base-button>
+      <base-button to="https://www.example.com" danger disabled>
+        <heart-icon slot="post-icon" />Danger
+      </base-button>
+      <h4>Disabled</h4>
+      <base-button to="/signup">
+        <heart-icon slot="post-icon" />Primary
+      </base-button>
+      <base-button title="secondary" to="/signup" secondary>
+        <heart-icon slot="post-icon" />Secondary
+      </base-button>
+      <base-button to="/signup" tertiary>
+        <heart-icon slot="post-icon" />Tertiary
+      </base-button>
+      <base-button to="/signup" danger>
+        <heart-icon slot="post-icon" />Danger
+      </base-button>
+    </base-section>
+    <base-section :theme="theme.Light">Default Section</base-section>
+    <base-section :layout="layout.Large" :theme="theme.Dark"
+      >Dark Section</base-section
     >
-    <base-button to="https://www.example.com" tertiary>Tertiary</base-button>
-    <base-button to="/signup" danger>Danger</base-button>
-    <h4>Disabled</h4>
-    <base-button to="https://www.example.com" disabled>Primary</base-button>
-    <base-button
-      title="secondary"
-      to="https://www.example.com"
-      secondary
-      disabled
-      >Secondary</base-button
+    <base-section :layout="layout.Large" :theme="theme.Grey"
+      >Grey Section</base-section
     >
-    <base-button to="https://www.example.com" tertiary disabled
-      >Tertiary</base-button
-    >
-    <base-button to="https://www.example.com" danger disabled
-      >Danger</base-button
-    >
-    <h3>Left Icons</h3>
-    <base-button to="https://www.example.com">
-      <heart-icon slot="pre-icon" />Primary
-    </base-button>
-    <base-button title="secondary" to="https://www.example.com" secondary>
-      <heart-icon slot="pre-icon" />Secondary
-    </base-button>
-    <base-button to="https://www.example.com" tertiary>
-      <heart-icon slot="pre-icon" />Tertiary
-    </base-button>
-    <base-button to="https://www.example.com" danger>
-      <heart-icon slot="pre-icon" />Danger
-    </base-button>
-    <h3>Right Icons</h3>
-    <base-button to="https://www.example.com">
-      <heart-icon slot="post-icon" />Primary
-    </base-button>
-    <base-button title="secondary" to="https://www.example.com" secondary>
-      <heart-icon slot="post-icon" />Secondary
-    </base-button>
-    <base-button to="https://www.example.com" tertiary>
-      <heart-icon slot="post-icon" />Tertiary
-    </base-button>
-    <base-button to="https://www.example.com" danger>
-      <heart-icon slot="post-icon" />Danger
-    </base-button>
-    <br />
-    <br />
-    <base-button to="https://www.example.com" disabled>
-      <heart-icon slot="post-icon" />Primary
-    </base-button>
-    <base-button
-      title="secondary"
-      to="https://www.example.com"
-      secondary
-      disabled
-    >
-      <heart-icon slot="post-icon" />Secondary
-    </base-button>
-    <base-button to="https://www.example.com" tertiary disabled>
-      <heart-icon slot="post-icon" />Tertiary
-    </base-button>
-    <base-button to="https://www.example.com" danger disabled>
-      <heart-icon slot="post-icon" />Danger
-    </base-button>
-    <h4>Disabled</h4>
-    <base-button to="/signup">
-      <heart-icon slot="post-icon" />Primary
-    </base-button>
-    <base-button title="secondary" to="/signup" secondary>
-      <heart-icon slot="post-icon" />Secondary
-    </base-button>
-    <base-button to="/signup" tertiary>
-      <heart-icon slot="post-icon" />Tertiary
-    </base-button>
-    <base-button to="/signup" danger>
-      <heart-icon slot="post-icon" />Danger
-    </base-button>
-  </base-section>
+    <base-section :layout="layout.Large">Large Section</base-section>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "nuxt-property-decorator";
 import BaseButton from "~/components/BaseButton.vue";
-import BaseSection from "~/components/BaseSection.vue";
+import BaseSection, { Layouts, Themes } from "~/components/BaseSection.vue";
 import HeartIcon from "~/assets/svg/heart.svg";
 
 @Component({
@@ -121,6 +135,9 @@ import HeartIcon from "~/assets/svg/heart.svg";
   }
 })
 export default class extends Vue {
+  layout = Layouts;
+  theme = Themes;
+
   test() {
     console.log("test");
   }
