c3f7ed5f228fff11867095759f322c988ed5fe10
Zane Milakovic <zane.milakovic@icloud.com>
2019-05-15 20:09:07 -0500

    updated styleguide with more component items

--- www/pages/styleguide.vue
+++ www/pages/styleguide.vue
@@ -17,6 +17,16 @@
       >
       <base-button overpanel="Default" tertiary disabled>Tertiary</base-button>
       <base-button overpanel="Default" danger disabled>Danger</base-button>
+      <h4>Small Buttons</h4>
+      <base-button overpanel="Default" small>Primary</base-button>
+      <base-button overpanel="Default" secondary small>Secondary</base-button>
+      <base-button overpanel="Default" tertiary small>Tertiary</base-button>
+      <base-button overpanel="Default" danger small>Danger</base-button>
+      <h4>Large Buttons</h4>
+      <base-button overpanel="Default" large>Primary</base-button>
+      <base-button overpanel="Default" secondary large>Secondary</base-button>
+      <base-button overpanel="Default" tertiary large>Tertiary</base-button>
+      <base-button overpanel="Default" danger large>Danger</base-button>
       <h3>Custom Events</h3>
       <base-button @click.native="test">Primary</base-button>
       <base-button @click.native="test" secondary>Secondary</base-button>
@@ -79,6 +89,7 @@
       </base-button>
       <br />
       <br />
+      <h4>Disabled</h4>
       <base-button to="https://www.example.com" disabled>
         <heart-icon slot="post-icon" />Primary
       </base-button>
@@ -96,7 +107,7 @@
       <base-button to="https://www.example.com" danger disabled>
         <heart-icon slot="post-icon" />Danger
       </base-button>
-      <h4>Disabled</h4>
+      <h4>Nuxt Route w/ Icon</h4>
       <base-button to="/signup">
         <heart-icon slot="post-icon" />Primary
       </base-button>
@@ -109,22 +120,48 @@
       <base-button to="/signup" danger>
         <heart-icon slot="post-icon" />Danger
       </base-button>
+      <h4>Small Buttons w/ Icons</h4>
+      <base-button overpanel="Default" small>
+        <heart-icon slot="post-icon" />Primary
+      </base-button>
+      <base-button overpanel="Default" secondary small>
+        <heart-icon slot="post-icon" />Secondary
+      </base-button>
+      <base-button overpanel="Default" tertiary small>
+        <heart-icon slot="post-icon" />Tertiary
+      </base-button>
+      <base-button overpanel="Default" danger small>
+        <heart-icon slot="post-icon" />Danger
+      </base-button>
+      <h4>Large Buttons w/ Icons</h4>
+      <base-button overpanel="Default" large>
+        <heart-icon slot="pre-icon" />Primary
+      </base-button>
+      <base-button overpanel="Default" secondary large>
+        <heart-icon slot="pre-icon" />Secondary
+      </base-button>
+      <base-button overpanel="Default" tertiary large>
+        <heart-icon slot="pre-icon" />Tertiary
+      </base-button>
+      <base-button overpanel="Default" danger large>
+        <heart-icon slot="pre-icon" />Danger
+      </base-button>
     </base-section>
     <base-section :theme="theme.Light">Default Section</base-section>
-    <base-section :layout="layout.Large" :theme="theme.Dark"
+    <base-section :layout="layout.Thin" :theme="theme.Dark"
       >Dark Section</base-section
     >
-    <base-section :layout="layout.Large" :theme="theme.Grey"
+    <base-section :layout="layout.Tall" :theme="theme.Grey"
       >Grey Section</base-section
     >
-    <base-section :layout="layout.Large">Large Section</base-section>
+    <base-section :layout="layout.Tall">Large Section</base-section>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "nuxt-property-decorator";
 import BaseButton from "~/components/BaseButton.vue";
-import BaseSection, { Layouts, Themes } from "~/components/BaseSection.vue";
+import BaseSection, { Layout, Theme } from "~/components/BaseSection.vue";
 import HeartIcon from "~/assets/svg/heart.svg";
 
 @Component({
@@ -135,8 +172,8 @@ import HeartIcon from "~/assets/svg/heart.svg";
   }
 })
 export default class extends Vue {
-  layout = Layouts;
-  theme = Themes;
+  layout = Layout;
+  theme = Theme;
 
   test() {
     console.log("test");
