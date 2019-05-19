3ea5158ebbf43672a9e3ecebea58268a7011833a
Zane Milakovic <zane.milakovic@icloud.com>
2019-05-18 21:02:06 -0500

    Adds TheHeader as Storybook story

---
+++ www/stories/TheHeader.stories.ts
@@ -0,0 +1,16 @@
+import { storiesOf } from "@storybook/vue";
+import TheHeader from "../components/TheHeader.vue";
+import HorizontalLogo from "../assets/svg/logo-horizontal-on-light.svg";
+import CloseIcon from "../assets/svg/close.svg";
+import MenuIcon from "../assets/svg/menu.svg";
+
+const stories = storiesOf("The Header", module);
+
+stories.add(
+  "Default",
+  () =>
+    ({
+      components: { TheHeader, HorizontalLogo, CloseIcon, MenuIcon },
+      template: `<the-header />`
+    } as object)
+);
