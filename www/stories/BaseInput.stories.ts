051bd4ee291b186dd746008a5a82738a3f4f066f
Zane Milakovic <zane.milakovic@icloud.com>
2019-05-20 15:59:53 -0500

    cleaned up BaseInput story

--- www/stories/BaseInput.stories.ts
+++ www/stories/BaseInput.stories.ts
@@ -65,17 +65,3 @@ stories.add(
   `
     } as object)
 );
-
-// storiesOf("Base Input/Types", module)
-//   .add("Text", () => '<base-input id="text-1">Text</base-input>')
-//   .add("Email", () => '<base-input id="email-1" type="email" v-model="email">Email</base-input>')
-//   .add("Hidden", () => '<base-input id="hidden-1" type="hidden">Hidden</base-input>')
-//   .add("Number", () => '<base-input id="number-1" type="number">Number</base-input>')
-//   .add("Password", () => '<base-input id="password-1" type="password">Password</base-input>')
-//   .add("Telephone", () => '<base-input id="tel-1" type="tel">Telephone</base-input>')
-
-// storiesOf("Base Input/Modifiers", module)
-//   .add("Hidden Label", () => '<base-input id="text-2" hidden-label>Hidden Label</base-input>')
-//   .add("Internal Label", () => '<base-input id="text-3" internal-label v-model="innerText">Inline Label</base-input>')
-//   .add("Placeholder", () => '<base-input id="text-4" v-model="placeholderText" placeholder="John Doe">Placeholder</base-input>')
-//   .add("Placeholder w/ Internal Label", () => '<base-input id="text-4" v-model="placeholderText" placeholder="John Doe" internal-label>Internal Label</base-input>')
