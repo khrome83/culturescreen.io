c1f2894b3369f39f1f8c72e628794b734dd8aa24
Zane Milakovic <zane.milakovic@icloud.com>
2019-05-23 17:42:58 -0500

    Closes #3 by adding Base Badge

---
+++ www/components/BaseBadge.vue
@@ -0,0 +1,56 @@
+<template>
+  <span class="badge" :class="{ secondary, small, large }">
+    <slot></slot>
+  </span>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "nuxt-property-decorator";
+
+@Component({
+  inheritAttrs: false
+})
+export default class BaseBadge extends Vue {
+  @Prop(Boolean) private secondary!: boolean;
+  @Prop(Boolean) private small!: boolean;
+  @Prop(Boolean) private large!: boolean;
+}
+</script>
+
+<style scoped>
+.badge {
+  display: inline-block;
+  text-align: center;
+  color: #fff;
+  fill: #fff;
+  background-color: #ee0028;
+  border-radius: 2px;
+  padding: 4px 6px;
+  text-decoration: none;
+  font-size: 16px;
+  font-weight: 300;
+  line-height: 1.1;
+  letter-spacing: 0.025rem;
+  text-transform: uppercase;
+}
+
+.secondary {
+  background-color: #010b19;
+}
+
+.small {
+  font-size: 14px;
+  padding: 4px 8px;
+  border-radius: 16px;
+  line-height: 1;
+  font-weight: 300;
+  letter-spacing: 1.25;
+}
+
+.large {
+  font-size: 20px;
+  padding: 6px 12px;
+  letter-spacing: 0.1rem;
+  line-height: 1;
+}
+</style>
---
+++ www/stories/BaseBadge.stories.ts
@@ -0,0 +1,56 @@
+import { storiesOf, addDecorator } from "@storybook/vue";
+import { radios, text, boolean } from "@storybook/addon-knobs";
+import centered from "@storybook/addon-centered/vue";
+import BaseBadge from "../components/BaseBadge.vue";
+import addonAPI from "@storybook/addons";
+const stories = storiesOf("Base Badge", module).addDecorator(centered);
+
+
+addonAPI.getChannel().on("storybook/background/update", bg => {
+  console.log("Background color", bg.selected);
+  console.log("Background name", bg.name);
+});
+
+stories.add(
+  "Default",
+  () =>
+    ({
+      components: { BaseBadge },
+      props: {
+        text: {
+          default: text("Text", "NEW")
+        },
+        theme: {
+          default: radios(
+            "Theme",
+            {
+              Primary: "primary",
+              Secondary: "secondary"
+            },
+            "primary"
+          )
+        },
+        size: {
+          default: radios(
+            "Size",
+            {
+              Small: "small",
+              Normal: "normal",
+              Large: "large"
+            },
+            "normal"
+          )
+        }
+      },
+      template: `
+        <base-badge v-bind="{
+          secondary: theme === 'secondary',
+          small: size === 'small',
+          large: size === 'large',
+        }">
+          {{text}}
+        </base-badge>
+      `
+    } as object)
+);
+
