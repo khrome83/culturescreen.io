b481ad29e5a4ad2d89188ffc01d2b131ec85e1c8
Zane Milakovic <zane.milakovic@icloud.com>
2019-05-24 19:19:14 -0500

    moves decarator to a indepdent file

--- www/stories/Styleguide.stories.ts
+++ www/stories/Styleguide.stories.ts
@@ -1,48 +1,9 @@
 import { storiesOf } from "@storybook/vue";
+import sectionStates from "./decorators/sectionStates";
 import HorizontalLogo from "../assets/svg/logo-horizontal-on-light.svg";
 import VerticalLogo from "../assets/svg/logo-vertical-on-light.svg";
 import CloseIcon from "../assets/svg/close.svg";
 import MenuIcon from "../assets/svg/menu.svg";
-
-const sectionStates = () => ({
-  data: () => ({
-    wrapper: {
-      margin: '0 2rem 2rem',
-      border: 'thin solid transparent',
-      boxShadow: 'rgba(0, 0, 0, 0.15) 0rem 0.125rem 0.3125rem 0rem',
-      borderRadius: '0.3125rem',
-      padding: '0.25rem 1rem',
-    },
-    light: {
-      backgroundColor: '#ffffff',
-    },
-    grey: {
-      backgroundColor: '#fdfcfb',
-      boxShadow: 'rgba(0, 0, 0, 0.2) 0rem 0.125rem 0.3125rem 0rem',
-    },
-    dark: {
-      backgroundColor: '#010b19',
-      boxShadow: 'rgba(0, 0, 0, 0.5) 0rem 0.125rem 0.3125rem 0rem',
-    },
-    heading: {
-      fontSize: '0.75rem',
-      margin: '0',
-      padding: '0.5rem 0 0.5rem 2rem',
-      color: '#737373',
-      textTransform: 'uppercase',
-    },
-  }),
-  template: `
-    <div>
-      <div :style="heading">On Light Background</div>
-      <div class="__bg-light" :style="[wrapper, light]"><story/></div>
-      <div :style="heading">On Gray Background</div>
-      <div class="__bg-grey" :style="[wrapper, grey]"><story/></div>
-      <div :style="heading">On Dark Background</div>
-      <div class="__bg-dark" :style="[wrapper, dark]"><story/></div>
-    </div>
-    `,
-});
 
 // Headings
 const headings = storiesOf("Styleguide/Headings", module).addDecorator(sectionStates)
