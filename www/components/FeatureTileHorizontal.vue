32c3ccf2a7b86577e9c9b328b70d2e4fa668377d
Zane Milakovic <zane.milakovic@icloud.com>
2019-05-28 07:38:41 -0500

    Closes #50 - Adds FeatureTileHorizontal

---
+++ www/components/FeatureTileHorizontal.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="feature-tile-horizontal">
+    <div v-if="$slots.default !== undefined" class="icon">
+      <slot></slot>
+    </div>
+    <div v-else class="checkmark">
+      <checkmark-icon/>
+    </div>
+    <div class="text-container">
+      <component :is="headingLevel" class="heading">{{heading}}</component>
+      <component :is="parsedText"></component>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "nuxt-property-decorator";
+import { parseLinks } from "~/library";
+import CheckmarkIcon from "~/assets/svg/checkmark.svg";
+
+@Component({
+  components: { CheckmarkIcon }
+})
+export default class FeatureTileHorizontal extends Vue {
+  @Prop({ required: true }) heading!: string;
+  @Prop({ default: 3, type: Number }) level!: number;
+  @Prop({ required: true }) text!: string;
+
+  get parsedText() {
+    return {
+      template: `<p class="feature-text">
+        ${parseLinks(this.text)}
+      </p>`
+    };
+  }
+
+  get headingLevel() {
+    const headings = ["h1", "h2", "h3", "h4"];
+    return !this.level || this.level > 4
+      ? headings[2]
+      : headings[this.level - 1];
+  }
+}
+</script>
+
+<style scoped>
+.feature-tile-horizontal {
+  padding: 1rem;
+  text-align: left;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
+  max-width: 30rem;
+}
+
+.text-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  padding-left: 2rem;
+}
+
+.checkmark > svg {
+  width: 2rem;
+  height: 2rem;
+  fill: #ee0028;
+}
+
+/* Grey Modifications - .checkmark > svg  */
+.__bg-grey .checkmark > svg {
+  fill: #da0629;
+}
+
+/* Dark Modifications - .checkmark > svg   */
+.__bg-dark .checkmark > svg {
+  fill: #f32144;
+}
+
+.icon > svg {
+  width: 6rem;
+  height: 6rem;
+}
+
+.icon .graphic-primary {
+  fill: #ee0028;
+}
+
+/* Grey Modifications - .icon .graphic-priamry */
+.__bg-grey .icon .graphic-primary {
+  fill: #da0629;
+}
+
+/* Dark Modifications - .icon .graphic-priamry  */
+.__bg-dark .icon .graphic-primary {
+  fill: #f32144;
+}
+
+.icon .graphic-secondary {
+  fill: #cccbcb;
+}
+
+/* Grey Modifications - .icon .graphic-secondary */
+.__bg-grey .icon .graphic-secondary {
+  fill: #cccbcb;
+}
+
+/* Dark Modifications - .icon .graphic-secondary  */
+.__bg-dark .icon .graphic-secondary {
+  fill: #737373;
+}
+
+.heading {
+  margin: 0;
+}
+
+.feature-text {
+  margin-bottom: 0;
+}
+</style>
---
+++ www/stories/FeatureTileHorizontal.stories.ts
@@ -0,0 +1,64 @@
+import { storiesOf, addDecorator } from "@storybook/vue";
+import { select, number, radios, text, boolean } from "@storybook/addon-knobs";
+import nanoid from "nanoid";
+import sectionStates from "./decorators/sectionStates";
+import FeatureTileHorizontal from "../components/FeatureTileHorizontal.vue";
+import Graphic from "../assets/svg/samples/video.svg";
+
+const stories = storiesOf("Marketing/Feature Tile Horizontal", module).addDecorator(sectionStates);
+
+stories.add(
+  "Icon",
+  () =>
+    ({
+      components: { FeatureTileHorizontal, Graphic },
+      props: {
+        heading: {
+          default: text("Heading", "Fast Playback")
+        },
+        text: {
+          default: text("Text", "World class video CDN delivers HD quality video. Check out these [[/path/to/benchmarks|benchmarks]] to learn more.")
+        },
+        level: {
+          default: number("Level (1)", 3, {
+            range: true,
+            min: 1,
+            max: 4,
+            step: 1
+          })
+        },
+      },
+      template: `
+        <feature-tile-horizontal :heading="heading" :text="text" :level="level">
+          <graphic />
+        </feature-tile-horizontal>
+      `
+    } as object)
+);
+
+stories.add(
+  "Checkmark",
+  () =>
+    ({
+      components: { FeatureTileHorizontal, Graphic },
+      props: {
+        heading: {
+          default: text("Heading", "Fast Playback")
+        },
+        text: {
+          default: text("Text", "World class video CDN delivers HD quality video. Check out these [[/path/to/benchmarks|benchmarks]] to learn more.")
+        },
+        level: {
+          default: number("Level (1)", 3, {
+            range: true,
+            min: 1,
+            max: 4,
+            step: 1
+          })
+        },
+      },
+      template: `
+        <feature-tile-horizontal :heading="heading" :text="text" :level="level" />
+      `
+    } as object)
+);
