4b71a3d8f1d8bd11af1740e9f5d51a2b340157d8
Zane Milakovic <zane.milakovic@icloud.com>
2019-05-27 22:05:43 -0500

    Moves TheHeader and TheFooter to marketing section

--- www/stories/TheFooter.stories.ts
+++ www/stories/TheFooter.stories.ts
@@ -2,7 +2,7 @@ import { storiesOf } from "@storybook/vue";
 import TheFooter from "../components/TheFooter.vue";
 import VerticalLogo from "../assets/svg/logo-vertical-on-light.svg";
 
-const stories = storiesOf("Application/The Footer", module);
+const stories = storiesOf("Marketing/The Footer", module);
 
 stories.add(
   "Default",
--- www/stories/TheHeader.stories.ts
+++ www/stories/TheHeader.stories.ts
@@ -4,7 +4,7 @@ import HorizontalLogo from "../assets/svg/logo-horizontal-on-light.svg";
 import CloseIcon from "../assets/svg/close.svg";
 import MenuIcon from "../assets/svg/menu.svg";
 
-const stories = storiesOf("Application/The Header", module);
+const stories = storiesOf("Marketing/The Header", module);
 
 stories.add(
   "Default",
