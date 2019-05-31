c7ea1bbb2d748a625181809a1901d76d6288326f
Zane Milakovic <zane.milakovic@icloud.com>
2019-05-30 21:26:00 -0500

    Closes #38 - Adds PricingTable component

---
+++ www/components/PricingTable.vue
@@ -0,0 +1,174 @@
+<template>
+  <div class="pricing-table" :class="{ promoted }">
+    <div class="icon">
+      <slot name="graphic"></slot>
+    </div>
+    <h3 class="heading">{{heading}}</h3>
+    <div class="price-set">
+      <span class="currency">{{currency}}</span>
+      <span class="price">{{price}}</span>
+      {{frequency}}
+    </div>
+    <component :is="parsedText"></component>
+    <div class="list">
+      <slot name="list"></slot>
+    </div>
+    <div class="cta">
+      <base-button :to="route" :secondary="!promoted" large>{{ buttonLabel }}</base-button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "nuxt-property-decorator";
+import BaseButton from "~/components/BaseButton.vue";
+import { parseLinks } from "~/library";
+
+@Component({
+  components: { BaseButton }
+})
+export default class PricingTable extends Vue {
+  @Prop({ required: true }) heading!: string;
+  @Prop({ required: true }) identifier!: string;
+  @Prop({ required: true }) text!: string;
+  @Prop({ required: true }) buttonLabel!: string;
+  @Prop({ default: "$" }) currency!: string;
+  @Prop({ required: true }) frequency!: string;
+  @Prop({ required: true, type: Number }) price!: number;
+  @Prop(Boolean) private promoted!: boolean;
+
+  get parsedText() {
+    return {
+      template: `<p class="feature-text">
+        ${parseLinks(this.text)}
+      </p>`
+    };
+  }
+
+  get route() {
+    return `/signup?plan=${this.identifier}`;
+  }
+}
+</script>
+
+<style scoped>
+.pricing-table {
+  padding: 2rem;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  max-width: 30rem;
+  border: thin solid #eaeaea;
+  border-radius: 0.25rem;
+}
+
+/* Grey Modifications - .pricing-table */
+.__bg-grey .pricing-table {
+  border-color: #cccbcb;
+}
+
+/* Dark Modifications - .pricing-table */
+.__bg-dark .pricing-table {
+  border-color: #33333c;
+}
+
+.promoted {
+  box-shadow: 0px 0px 0.5rem 0.25rem rgba(0, 0, 0, 0.05);
+}
+
+/* Grey Modifications - .promoted */
+.__bg-grey .promoted {
+  background-color: #fff;
+}
+
+/* Dark Modifications - .promoted */
+.__bg-dark .promoted {
+  background-color: #000;
+  box-shadow: 0px 0px 0.5rem 0.25rem rgba(255, 255, 255, 0.1);
+}
+
+.icon > svg {
+  width: 6rem;
+  height: 6rem;
+}
+
+.icon .graphic-primary {
+  fill: #ee0028;
+}
+
+/* Grey Modifications - .icon .graphic-priamry */
+.__bg-grey .icon .graphic-primary {
+  fill: #da0629;
+}
+
+/* Dark Modifications - .icon .graphic-priamry */
+.__bg-dark .icon .graphic-primary {
+  fill: #f32144;
+}
+
+.icon .graphic-secondary {
+  fill: #cccbcb;
+}
+
+/* Grey Modifications - .icon .graphic-secondary */
+.__bg-grey .icon .graphic-secondary {
+  fill: #cccbcb;
+}
+
+/* Dark Modifications - .icon .graphic-secondary */
+.__bg-dark .icon .graphic-secondary {
+  fill: #737373;
+}
+
+.heading {
+  margin: 0 0 1rem;
+  text-align: center;
+}
+
+.feature-text {
+  margin-bottom: 0;
+}
+
+.price-set {
+  font-size: 2.25rem;
+  line-height: 1.15;
+  margin: 0 0 2rem;
+  padding: 0 0 2rem;
+  width: 100%;
+  border-bottom: thin solid #eaeaea;
+}
+
+/* Grey Modifications - .price-set */
+.__bg-grey .price-set {
+  border-color: #cccbcb;
+}
+
+/* Dark Modifications - .price-set */
+.__bg-dark .price-set {
+  border-color: #33333c;
+}
+
+.price {
+  font-size: 3.5rem;
+  line-height: 1;
+  font-weight: 400;
+  font-family: "Raleway", sans-serif;
+}
+
+.list {
+  text-align: left;
+  width: 100%;
+  padding: 1rem 2rem;
+  box-sizing: border-box;
+}
+
+.cta {
+  margin-top: 2rem;
+}
+
+.cta > .button {
+  min-width: 12.5rem;
+}
+</style>
---
+++ www/stories/PricingTable.stories.ts
@@ -0,0 +1,67 @@
+import { storiesOf, addDecorator } from "@storybook/vue";
+import { select, number, radios, text, boolean } from "@storybook/addon-knobs";
+import nanoid from "nanoid";
+import sectionStates from "./decorators/sectionStates";
+import PricingTable from "../components/PricingTable.vue";
+import Graphic from "../assets/svg/samples/video.svg";
+
+const stories = storiesOf("Marketing/Pricing Table", module).addDecorator(sectionStates);
+
+stories.add(
+  "Default",
+  () =>
+    ({
+      components: { PricingTable, Graphic },
+      props: {
+        identifier: {
+          default: text("Identifier", "bronze_01")
+        },
+        heading: {
+          default: text("Heading", "Bronze")
+        },
+        promoted: {
+          default: boolean("Promoted", false)
+        },
+        text: {
+          default: text("Text", "The ideal plan for small businesses that are growing slowly, or just starting out.")
+        },
+        currency: {
+          default: text("Currency", "$")
+        },
+        price: {
+          default: number("Price", 35)
+        },
+        frequency: {
+          default: text("Frequency", " / month")
+        },
+        buttonLabel: {
+          default: text("Button Label", "Start Now")
+        },
+      },
+      template: `
+        <pricing-table v-bind="{ identifier, heading, promoted, text, currency, price, frequency, buttonLabel }">
+          <template v-slot:graphic>
+            <graphic />
+          </template>
+          <template v-slot:list>
+            <ul>
+              <li>1 Job</li>
+              <li>Unlimtied One-Way Interviews</li>
+              <li>10 User Seats</li>
+              <li>Branding</li>
+              <li>Rating System</li>
+              <li>Interview Tagging</li>
+              <li>Company Profile</li>
+              <li>Rejection Management</li>
+              <li>Grandular User Permissions</li>
+              <li>Company Intro Video</li>
+              <li>Company Outro Video</li>
+              <li>Question Bank</li>
+              <li>Custom Questions</li>
+              <li>Think Time</li>
+            </ul>
+          </template>
+        </pricing-table>
+      `
+    } as object)
+);
