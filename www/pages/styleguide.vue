2915a57ce01b7f0c07c6551b3c21d2b1d585df75
Zane Milakovic <zane.milakovic@icloud.com>
2019-06-02 07:56:22 -0500

    Adds inline text to test input

--- www/pages/styleguide.vue
+++ www/pages/styleguide.vue
@@ -12,9 +12,7 @@
       <base-button overpanel="Default" danger>Danger</base-button>
       <h4>Disabled</h4>
       <base-button overpanel="Default" disabled>Primary</base-button>
-      <base-button overpanel="Default" secondary disabled
-        >Secondary</base-button
-      >
+      <base-button overpanel="Default" secondary disabled>Secondary</base-button>
       <base-button overpanel="Default" tertiary disabled>Tertiary</base-button>
       <base-button overpanel="Default" danger disabled>Danger</base-button>
       <h4>Small Buttons</h4>
@@ -34,237 +32,170 @@
       <base-button @click.native="test" danger>Danger</base-button>
       <h4>Disabled</h4>
       <base-button @click.native="test" disabled>Primary</base-button>
-      <base-button @click.native="test" secondary disabled
-        >Secondary</base-button
-      >
+      <base-button @click.native="test" secondary disabled>Secondary</base-button>
       <base-button @click.native="test" tertiary disabled>Tertiary</base-button>
       <base-button @click.native="test" danger disabled>Danger</base-button>
       <h3>External Link</h3>
       <base-button to="https://www.example.com">Primary</base-button>
-      <base-button title="secondary" to="https://www.example.com" secondary
-        >Secondary</base-button
-      >
+      <base-button title="secondary" to="https://www.example.com" secondary>Secondary</base-button>
       <base-button to="https://www.example.com" tertiary>Tertiary</base-button>
       <base-button to="/signup" danger>Danger</base-button>
       <h4>Disabled</h4>
       <base-button to="https://www.example.com" disabled>Primary</base-button>
-      <base-button
-        title="secondary"
-        to="https://www.example.com"
-        secondary
-        disabled
-        >Secondary</base-button
-      >
-      <base-button to="https://www.example.com" tertiary disabled
-        >Tertiary</base-button
-      >
-      <base-button to="https://www.example.com" danger disabled
-        >Danger</base-button
-      >
+      <base-button title="secondary" to="https://www.example.com" secondary disabled>Secondary</base-button>
+      <base-button to="https://www.example.com" tertiary disabled>Tertiary</base-button>
+      <base-button to="https://www.example.com" danger disabled>Danger</base-button>
       <h3>Left Icons</h3>
       <base-button to="https://www.example.com">
         <template v-slot:pre-icon>
-          <heart-icon aria-hidden="true" />
+          <heart-icon aria-hidden="true"/>
         </template>
-        <template v-slot:default
-          >Primary</template
-        >
+        <template v-slot:default>Primary</template>
       </base-button>
       <base-button title="secondary" to="https://www.example.com" secondary>
         <template v-slot:pre-icon>
-          <heart-icon aria-hidden="true" />
+          <heart-icon aria-hidden="true"/>
         </template>
-        <template v-slot:default
-          >Secondary</template
-        >
+        <template v-slot:default>Secondary</template>
       </base-button>
       <base-button to="https://www.example.com" tertiary>
         <template v-slot:pre-icon>
-          <heart-icon aria-hidden="true" />
+          <heart-icon aria-hidden="true"/>
         </template>
-        <template v-slot:default
-          >Tertiary</template
-        >
+        <template v-slot:default>Tertiary</template>
       </base-button>
       <base-button to="https://www.example.com" danger>
         <template v-slot:pre-icon>
-          <heart-icon aria-hidden="true" />
+          <heart-icon aria-hidden="true"/>
         </template>
-        <template v-slot:default
-          >Danger</template
-        >
+        <template v-slot:default>Danger</template>
       </base-button>
       <h3>Right Icons</h3>
       <base-button to="https://www.example.com">
         <template v-slot:post-icon>
-          <heart-icon aria-hidden="true" />
+          <heart-icon aria-hidden="true"/>
         </template>
-        <template v-slot:default
-          >Primary</template
-        >
+        <template v-slot:default>Primary</template>
       </base-button>
       <base-button title="secondary" to="https://www.example.com" secondary>
         <template v-slot:post-icon>
-          <heart-icon aria-hidden="true" />
+          <heart-icon aria-hidden="true"/>
         </template>
-        <template v-slot:default
-          >Secondary</template
-        >
+        <template v-slot:default>Secondary</template>
       </base-button>
       <base-button to="https://www.example.com" tertiary>
         <template v-slot:post-icon>
-          <heart-icon aria-hidden="true" />
+          <heart-icon aria-hidden="true"/>
         </template>
-        <template v-slot:default
-          >Tertiary</template
-        >
+        <template v-slot:default>Tertiary</template>
       </base-button>
       <base-button to="https://www.example.com" danger>
         <template v-slot:post-icon>
-          <heart-icon aria-hidden="true" />
+          <heart-icon aria-hidden="true"/>
         </template>
-        <template v-slot:default
-          >Danger</template
-        >
+        <template v-slot:default>Danger</template>
       </base-button>
-      <br />
-      <br />
+      <br>
+      <br>
       <h4>Disabled</h4>
       <base-button to="https://www.example.com" disabled>
         <template v-slot:post-icon>
-          <heart-icon aria-hidden="true" />
+          <heart-icon aria-hidden="true"/>
         </template>
-        <template v-slot:default
-          >Primary</template
-        >
+        <template v-slot:default>Primary</template>
       </base-button>
-      <base-button
-        title="secondary"
-        to="https://www.example.com"
-        secondary
-        disabled
-      >
+      <base-button title="secondary" to="https://www.example.com" secondary disabled>
         <template v-slot:post-icon>
-          <heart-icon aria-hidden="true" />
+          <heart-icon aria-hidden="true"/>
         </template>
-        <template v-slot:default
-          >Secondary</template
-        >
+        <template v-slot:default>Secondary</template>
       </base-button>
       <base-button to="https://www.example.com" tertiary disabled>
         <template v-slot:post-icon>
-          <heart-icon aria-hidden="true" />
+          <heart-icon aria-hidden="true"/>
         </template>
-        <template v-slot:default
-          >Tertiary</template
-        >
+        <template v-slot:default>Tertiary</template>
       </base-button>
       <base-button to="https://www.example.com" danger disabled>
         <template v-slot:post-icon>
-          <heart-icon aria-hidden="true" />
+          <heart-icon aria-hidden="true"/>
         </template>
-        <template v-slot:default
-          >Danger</template
-        >
+        <template v-slot:default>Danger</template>
       </base-button>
       <h4>Nuxt Route w/ Icon</h4>
       <base-button to="/signup">
         <template v-slot:post-icon>
-          <heart-icon aria-hidden="true" />
+          <heart-icon aria-hidden="true"/>
         </template>
-        <template v-slot:default
-          >Primary</template
-        >
+        <template v-slot:default>Primary</template>
       </base-button>
       <base-button title="secondary" to="/signup" secondary>
         <template v-slot:post-icon>
-          <heart-icon aria-hidden="true" />
+          <heart-icon aria-hidden="true"/>
         </template>
-        <template v-slot:default
-          >Secondary</template
-        >
+        <template v-slot:default>Secondary</template>
       </base-button>
       <base-button to="/signup" tertiary>
         <template v-slot:post-icon>
-          <heart-icon aria-hidden="true" />
+          <heart-icon aria-hidden="true"/>
         </template>
-        <template v-slot:default
-          >Tertiary</template
-        >
+        <template v-slot:default>Tertiary</template>
       </base-button>
       <base-button to="/signup" danger>
         <template v-slot:post-icon>
-          <heart-icon aria-hidden="true" />
+          <heart-icon aria-hidden="true"/>
         </template>
-        <template v-slot:default
-          >Danger</template
-        >
+        <template v-slot:default>Danger</template>
       </base-button>
       <h4>Small Buttons w/ Icons</h4>
       <base-button overpanel="Default" small>
         <template v-slot:post-icon>
-          <heart-icon aria-hidden="true" />
+          <heart-icon aria-hidden="true"/>
         </template>
-        <template v-slot:default
-          >Primary</template
-        >
+        <template v-slot:default>Primary</template>
       </base-button>
       <base-button overpanel="Default" secondary small>
         <template v-slot:post-icon>
-          <heart-icon aria-hidden="true" />
+          <heart-icon aria-hidden="true"/>
         </template>
-        <template v-slot:default
-          >Secondary</template
-        >
+        <template v-slot:default>Secondary</template>
       </base-button>
       <base-button overpanel="Default" tertiary small>
         <template v-slot:post-icon>
-          <heart-icon aria-hidden="true" />
+          <heart-icon aria-hidden="true"/>
         </template>
-        <template v-slot:default
-          >Tertiary</template
-        >
+        <template v-slot:default>Tertiary</template>
       </base-button>
       <base-button overpanel="Default" danger small>
         <template v-slot:post-icon>
-          <heart-icon aria-hidden="true" />
+          <heart-icon aria-hidden="true"/>
         </template>
-        <template v-slot:default
-          >Danger</template
-        >
+        <template v-slot:default>Danger</template>
       </base-button>
       <h4>Large Buttons w/ Icons</h4>
       <base-button overpanel="Default" large>
         <template v-slot:pre-icon>
-          <heart-icon aria-hidden="true" />
+          <heart-icon aria-hidden="true"/>
         </template>
-        <template v-slot:default
-          >Primary</template
-        >
+        <template v-slot:default>Primary</template>
       </base-button>
       <base-button overpanel="Default" secondary large>
         <template v-slot:pre-icon>
-          <heart-icon aria-hidden="true" />
+          <heart-icon aria-hidden="true"/>
         </template>
-        <template v-slot:default
-          >Secondary</template
-        >
+        <template v-slot:default>Secondary</template>
       </base-button>
       <base-button overpanel="Default" tertiary large>
         <template v-slot:pre-icon>
-          <heart-icon aria-hidden="true" />
+          <heart-icon aria-hidden="true"/>
         </template>
-        <template v-slot:default
-          >Tertiary</template
-        >
+        <template v-slot:default>Tertiary</template>
       </base-button>
       <base-button overpanel="Default" danger large>
         <template v-slot:pre-icon>
-          <heart-icon aria-hidden="true" />
+          <heart-icon aria-hidden="true"/>
         </template>
-        <template v-slot:default
-          >Danger</template
-        >
+        <template v-slot:default>Danger</template>
       </base-button>
     </base-section>
     <base-section :layout="layout.Thin">
@@ -275,16 +206,12 @@
     <base-section :theme="theme.Grey">Grey Theme</base-section>
     <base-section :layout="layout.Tall">Tall Layout</base-section>
     <base-section :theme="theme.Grey">Default Layout</base-section>
-    <base-section :layout="layout.Thin" :theme="theme.Dark"
-      >Thin Layout</base-section
-    >
+    <base-section :layout="layout.Thin" :theme="theme.Dark">Thin Layout</base-section>
     <base-section>
       <h2>Form Fields</h2>
       <h3>Input</h3>
       <base-input id="text-1">Text</base-input>
-      <base-input id="email-1" :type="type.Email" v-model="email"
-        >Email</base-input
-      >
+      <base-input id="email-1" :type="type.Email" v-model="email">Email</base-input>
       <base-input id="hidden-1" :type="type.Hidden">Hidden</base-input>
       <base-input id="number-1" :type="type.Number">Number</base-input>
       <base-input id="password-1" :type="type.Password">Password</base-input>
@@ -292,21 +219,17 @@
       <h4>Hidden Label</h4>
       <base-input id="text-2" hidden-label>Hidden Label</base-input>
       <h4>Internal Label</h4>
-      <base-input id="text-3" internal-label v-model="innerText"
-        >Inline Label</base-input
-      >
+      <base-input id="text-3" internal-label v-model="innerText">Inline Label</base-input>
       <h4>Placeholder Attribute</h4>
-      <base-input id="text-4" v-model="placeholderText" placeholder="John Doe"
-        >Placeholder</base-input
-      >
+      <base-input id="text-4" v-model="placeholderText" placeholder="John Doe">Placeholder</base-input>
       <h4>Placeholder Attribute w/ Internal Label</h4>
       <base-input
         id="text-4"
         v-model="placeholderText"
         placeholder="John Doe"
         internal-label
-        >Internal Label</base-input
-      >
+        help-text="Testing [[/origin|Orign]]"
+      >Internal Label</base-input>
     </base-section>
   </div>
 </template>
