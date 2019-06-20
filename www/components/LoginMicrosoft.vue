6becf261a79b1c4ad5674fdbd6e49d9928dd6838
Zane Milakovic <zane.milakovic@icloud.com>
2019-06-19 21:20:30 -0500

    #83 - stubs out login microsoft to test flow

---
+++ www/components/LoginMicrosoft.vue
@@ -0,0 +1,34 @@
+<template>
+  <base-button class="login-microsoft" @click.native="login" secondary>Microsoft Sign In</base-button>
+</template>
+
+<script lang="ts">
+import { Action, Component, Prop, Vue } from "nuxt-property-decorator";
+import BaseButton from "~/components/BaseButton.vue";
+
+@Component({
+  components: { BaseButton }
+})
+export default class LoginMicrosoft extends Vue {
+  @Action("user/signInWithMicrosoft") userSignInMicrosoft;
+
+  formEmail = "";
+  formPassword = "";
+
+  async login() {
+    try {
+      await this.userSignInMicrosoft();
+      console.log("microsoft login");
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.login-microsoft {
+  margin-top: 1rem;
+  max-width: 10rem;
+}
+</style>
