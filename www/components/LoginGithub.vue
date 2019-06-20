604acb3753236ee7cb08bf1603cfc70d59cf11d3
Zane Milakovic <zane.milakovic@icloud.com>
2019-06-19 21:20:08 -0500

    #84 - stubs out login github component to test flow

---
+++ www/components/LoginGithub.vue
@@ -0,0 +1,34 @@
+<template>
+  <base-button class="login-github" @click.native="login" secondary>Github Sign In</base-button>
+</template>
+
+<script lang="ts">
+import { Action, Component, Prop, Vue } from "nuxt-property-decorator";
+import BaseButton from "~/components/BaseButton.vue";
+
+@Component({
+  components: { BaseButton }
+})
+export default class LoginGithub extends Vue {
+  @Action("user/signInWithGithub") userSignInGithub;
+
+  formEmail = "";
+  formPassword = "";
+
+  async login() {
+    try {
+      await this.userSignInGithub();
+      console.log("github login");
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.login-github {
+  margin-top: 1rem;
+  max-width: 10rem;
+}
+</style>
