44758bcb61ac95a269a6d73e3312b9205f9d8f5d
Zane Milakovic <zane.milakovic@icloud.com>
2019-06-19 21:28:09 -0500

    #81 - creates two authenticated pages for testing

---
+++ www/pages/app/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <section>
+    <base-section>
+      <h1 class="header">App (Protected Area)</h1>
+      <log-out/>
+      <a href="/app/test">Testing Anchor</a>
+      <nuxt-link to="/app/test">Testing SPA Link</nuxt-link>
+    </base-section>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+import { Jsonld } from "nuxt-jsonld";
+import BaseSection from "~/components/BaseSection.vue";
+import LogOut from "~/components/LogOut.vue";
+
+@Jsonld
+@Component({
+  components: { BaseSection, LogOut }
+})
+export default class extends Vue {
+  jsonld() {
+    return {
+      "@context": "https://schema.org",
+      "@type": "Organization",
+      url: "https://www.culturescreen.io",
+      logo: "https://www.culturescreen.io/logo.png",
+      contactPoint: [
+        {
+          "@type": "ContactPoint",
+          telephone: "+1-401-555-1212",
+          contactType: "customer service"
+        }
+      ]
+    };
+  }
+}
+</script>
+
+<style scoped></style>
