639504d41e07b249772b01d9b96dc4171f5f7821
Zane Milakovic <zane.milakovic@icloud.com>
2019-06-19 21:22:00 -0500

    #95 - Stubs out logout component

---
+++ www/components/LogOut.vue
@@ -0,0 +1,31 @@
+<template>
+  <base-button v-if="userIsAuthenticated" class="logout" @click.native="signOut">Log Out</base-button>
+</template>
+
+<script lang="ts">
+import { Getter, Action, Component, Prop, Vue } from "nuxt-property-decorator";
+import BaseButton from "~/components/BaseButton.vue";
+
+@Component({
+  components: { BaseButton }
+})
+export default class LogOut extends Vue {
+  @Getter("user/isAuthenticated") userIsAuthenticated;
+  @Action("user/signOut") userSignOut;
+
+  async signOut() {
+    try {
+      await this.userSignOut();
+      console.log("signed out");
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.logout {
+  line-height: inherit;
+}
+</style>
