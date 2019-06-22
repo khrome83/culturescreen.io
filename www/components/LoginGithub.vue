52f9c7cca3f740460f9702004611836bfc83a4ce
Zane Milakovic <zane.milakovic@icloud.com>
2019-06-22 15:42:47 -0500

    Closes #84 - Adds Github Login

--- www/components/LoginGithub.vue
+++ www/components/LoginGithub.vue
@@ -1,25 +1,36 @@
 <template>
-  <base-button class="login-github" @click.native="login" secondary>Github Sign In</base-button>
+  <base-button class="login-github" @click.native="login" :isLoading="loading" secondary>
+    <template v-slot:pre-icon>
+      <github-logo aria-hidden="true"/>
+    </template>
+    <template v-slot:default>Sign in with Github</template>
+  </base-button>
 </template>
 
 <script lang="ts">
 import { Action, Component, Prop, Vue } from "nuxt-property-decorator";
 import BaseButton from "~/components/BaseButton.vue";
+import GithubLogo from "~/assets/svg/brands/github.svg";
 
 @Component({
-  components: { BaseButton }
+  components: {
+    BaseButton,
+    GithubLogo
+  }
 })
 export default class LoginGithub extends Vue {
   @Action("user/signInWithGithub") userSignInGithub;
 
-  formEmail = "";
-  formPassword = "";
+  loading = false;
 
   async login() {
     try {
+      this.loading = true;
       await this.userSignInGithub();
+      this.loading = false;
       console.log("github login");
     } catch (e) {
+      this.loading = false;
       console.log(e.message);
     }
   }
@@ -29,6 +40,15 @@ export default class LoginGithub extends Vue {
 <style scoped>
 .login-github {
   margin-top: 1rem;
-  max-width: 10rem;
+  text-align: left;
+  padding: 0.5rem 1rem;
+  max-width: 16rem;
+}
+
+.login-github .text > svg {
+  height: 1.2rem;
+  width: 1.2rem;
+  transform: scale(1.2);
+  margin: 0 0.625rem -0.2rem 0;
 }
 </style>
